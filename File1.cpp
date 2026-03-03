4b36129e57037ab4857d81913ee204ba08acdb98
Pis23 <nafisshl10@gmail.com>
2026-03-03 20:22:42 +0700

    file1

---
+++ File1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+//deklarasi global
+int p, l, luas;
+
+//Implementasi prosedur dan fungsi
+void input(){
+    cout << "Masukkan panjang= ";
+    cin >> p;
+    cout << "Masukkan lebar= ";
+    cin >> l;
+}
+
+int luaspersegi(){
+    return p * l;
+}
+
+void output(){
+    cout << "Hasil luas= " << luaspersegi() << endl;
+    cout << "Terimakasih";
+}
+
+int main(){
+    input();
+    output();
+}//Selesai
